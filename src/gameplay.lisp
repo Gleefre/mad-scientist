c6eed8b120cec530a55462c38279fbb2463e72b7
Gleefre <varedif.a.s@gmail.com>
2023-11-13 18:30:59 +0100

    Move gameplay related trackers to the gameplay

--- src/gameplay.lisp
+++ src/gameplay.lisp
@@ -55,3 +55,26 @@
 (defun move* (&optional (speed 0.1))
   (a:when-let ((direction (last-dir)))
     (move direction speed)))
+
+;;; Trackers
+
+(defun init-trackers ()
+  (define-tracker :vertical (make-lp-tracker :scancode-up :scancode-down :scancode-w :scancode-s))
+  (define-tracker :horizontal (make-lp-tracker :scancode-left :scancode-right :scancode-a :scancode-d)))
+
+(defun last-dir ()
+  (case (last-pressed (tracker :vertical))
+    ((:scancode-w :scancode-up)
+     (case (last-pressed (tracker :horizontal))
+       ((:scancode-a :scancode-left) :upl)
+       ((:scancode-d :scancode-right) :upr)
+       (t :up)))
+    ((:scancode-s :scancode-down)
+     (case (last-pressed (tracker :horizontal))
+       ((:scancode-a :scancode-left) :downl)
+       ((:scancode-d :scancode-right) :downr)
+       (t :down)))
+    (t
+     (case (last-pressed (tracker :horizontal))
+       ((:scancode-a :scancode-left) :left)
+       ((:scancode-d :scancode-right) :right)))))
--- src/utils.lisp
+++ src/utils.lisp
@@ -31,26 +31,3 @@
 (defun pressed? (&rest scancodes)
   (loop for scancode in scancodes
         thereis (kit.sdl2:key-down-p *keyboard-tracker* scancode)))
-
-;;; Trackers
-
-(defun init-trackers ()
-  (define-tracker :vertical (make-lp-tracker :scancode-up :scancode-down :scancode-w :scancode-s))
-  (define-tracker :horizontal (make-lp-tracker :scancode-left :scancode-right :scancode-a :scancode-d)))
-
-(defun last-dir ()
-  (case (last-pressed (tracker :vertical))
-    ((:scancode-w :scancode-up)
-     (case (last-pressed (tracker :horizontal))
-       ((:scancode-a :scancode-left) :upl)
-       ((:scancode-d :scancode-right) :upr)
-       (t :up)))
-    ((:scancode-s :scancode-down)
-     (case (last-pressed (tracker :horizontal))
-       ((:scancode-a :scancode-left) :downl)
-       ((:scancode-d :scancode-right) :downr)
-       (t :down)))
-    (t
-     (case (last-pressed (tracker :horizontal))
-       ((:scancode-a :scancode-left) :left)
-       ((:scancode-d :scancode-right) :right)))))
