3c807e7aa492039605ef54cde0d63dcb9208860a
Gleefre <varedif.a.s@gmail.com>
2023-11-13 18:29:50 +0100

    Rewrite trackers

--- src/trackers.lisp
+++ src/trackers.lisp
@@ -1,45 +1,71 @@
 (in-package #:mad-scientist)
 
-;;; Trackers
+;;; Trackers namespace
 
-(defvar *keyboard-tracker*)
-(defvar *dir-trackers*)
+(defvar *trackers*)
 
-;; FIXME: move trackers to sketch-utils (maybe),
-;; make them ok, not with conses and lists (but with classes & methods instead)
-(defun make-direction-tracker (&rest scancodes)
-  (cons scancodes (list :tracker)))
+(defun symbol-tracker (name)
+  (gethash name *trackers*))
 
-(defun make-direction-trackers ()
-  (list (make-direction-tracker :scancode-up :scancode-w
-                                :scancode-down :scancode-s)
-        (make-direction-tracker :scancode-left :scancode-d
-                                :scancode-right :scancode-a)))
+(defun (setf symbol-tracker) (value name)
+  (setf (gethash name *trackers*) value))
 
-(defun last-in-direction-tracker (tracker)
-  (cadr (cdr tracker)))
+(defmacro tracker (name)
+  `(symbol-tracker ',name))
 
-(defun update-direction-tracker (tracker state scancode)
-  (let ((scancodes (car tracker))
-        (llist (cdr tracker)))
-    (when (member scancode scancodes)
+(defmacro define-tracker (name initform)
+  `(setf (tracker ,name) ,initform))
+
+;;; API for trackers
+
+(defclass tracker () ())
+
+(defgeneric update-tracker (tracker state scancode &key timestamp repeat-p keysym &allow-other-keys))
+
+;;; Default trackers
+
+;; kit.sdl2's tracker
+(defclass keystate-tracker (tracker kit.sdl2:keystate-tracker) ())
+
+(defmethod update-tracker ((tracker kit.sdl2:keystate-tracker) state scancode &key repeat-p keysym &allow-other-keys)
+  (kit.sdl2:keystate-update tracker state repeat-p keysym))
+
+(defun key-pressed-p (keystate-tracker scancode)
+  (kit.sdl2:key-down-p keystate-tracker scancode))
+
+;; "last-pressed" tracker
+(defclass last-pressed-tracker (tracker)
+  ((scancodes :initform () :initarg :scancodes :accessor lp-tracker-scancodes)
+   (%list :initform () :accessor %lp-list)))
+
+(defun make-lp-tracker (&rest scancodes)
+  (make-instance 'last-pressed-tracker :scancodes scancodes))
+
+(defmethod update-tracker (tracker state scancode &key repeat-p &allow-other-keys)
+  (unless repeat-p
+    (when (member scancode (lp-tracker-scancodes tracker))
       (ecase state
-        (:keydown (push scancode (cdr llist)))
-        (:keyup (a:removef (cdr llist) scancode))))))
+        (:keydown (push scancode (%lp-list tracker)))
+        (:keyup (a:removef (%lp-list tracker) scancode))))))
 
-(defun last-dir ()
-  (case (last-in-direction-tracker (car *dir-trackers*))
-    ((:scancode-w :scancode-up)
-     (case (last-in-direction-tracker (cadr *dir-trackers*))
-       ((:scancode-a :scancode-left) :upl)
-       ((:scancode-d :scancode-right) :upr)
-       (t :up)))
-    ((:scancode-s :scancode-down)
-     (case (last-in-direction-tracker (cadr *dir-trackers*))
-       ((:scancode-a :scancode-left) :downl)
-       ((:scancode-d :scancode-right) :downr)
-       (t :down)))
-    (t
-     (case (last-in-direction-tracker (cadr *dir-trackers*))
-       ((:scancode-a :scancode-left) :left)
-       ((:scancode-d :scancode-right) :right)))))
+(defun last-pressed (tracker)
+  (values (car (%lp-list tracker))
+          (not (null (%lp-list tracker)))))
+
+;;; Hook into the sketch
+
+(stealth-mixin:define-stealth-mixin sketch-trackers () s:sketch
+  ((%trackers :initform (make-hash-table) :accessor %trackers)))
+
+(defmethod s:draw :around ((sketch sketch-trackers) &key &allow-other-keys)
+  (let ((*trackers* (%trackers sketch)))
+    (call-next-method)))
+
+(defmethod s:setup :around ((sketch sketch-trackers) &key &allow-other-keys)
+  (let ((*trackers* (%trackers sketch)))
+    (call-next-method)))
+
+(defmethod kit.sdl2:keyboard-event :before ((window sketch-trackers) state ts rep? keysym)
+  (loop for tracker being the hash-value of (%trackers window)
+        do (update-tracker tracker state (sdl2:scancode keysym)
+                           :timestamp ts :repeat-p rep? :keysym keysym)))
